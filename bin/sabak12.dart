020f01a9bcaf93e3ec0943343bfeecb2e01a8976
Elchibek <abdylashymov@gmail.com>
2024-09-04 17:27:52 +0600

    12 lesson added

---
+++ bin/sabak12.dart
@@ -0,0 +1,43 @@
+List<String> textter = ['Kyrgyz', 'Kazak', 'Orus'];
+
+void main() {
+  for (String element in textter) {
+    print(element);
+  }
+}
+
+void san() {
+  1 + 1;
+  print('Hello World');
+}
+String text(){
+  String ulut = 'Kyrgyzs';
+  print(ulut);
+  return ulut;
+}
+
+int sandar(){
+  return 80;
+}
+
+var ea = 10;
+var b = 'Salam';
+
+String c = 'Hello World';
+String d = '90';
+
+int e = 30;
+
+double f = 30.906;
+double g = 12;
+
+num h = 22;
+num j = 15.7;
+
+bool k = false;
+bool m = true;
+
+dynamic n = 'Kalpak';
+dynamic o = 123;
+dynamic p = true;
+dynamic q = 34.6;
---
+++ test/sabak12_test.dart
@@ -0,0 +1,8 @@
+import 'package:sabak12/sabak12.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
